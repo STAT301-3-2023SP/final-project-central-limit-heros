b1b49ebeb30326786c5b97c8125281f06af16617
pascoesean <seanpascoe2024@u.northwestern.edu>
2023-04-24 18:46:00 -0500

    adding metadata for 2021

---
+++ R_scripts/parks_wrangling.R
@@ -0,0 +1,16 @@
+#############################################################################
+# DATA PROCESSING FOR NANDA PARKS DATA 2018 ----
+#############################################################################
+
+## loading packages + data ----
+
+library(tidyverse)
+
+nanda_parks <- read_csv("data/raw/nanda_parks_2018/nanda_parks_zcta_2018_01P.csv")
+
+gayta <- read_csv("data/raw/gay-ta.csv")
+
+inner_join(gayta, nanda_parks, by = c( "GEOID10" = "zcta19")) %>%
+  nrow()
+
+# all 
