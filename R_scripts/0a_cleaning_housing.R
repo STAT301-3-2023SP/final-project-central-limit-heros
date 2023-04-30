9fe9cdb165b86e65dfd1b8c577be6f54c2ee7f06
vnevirkov <you@example.com>
2023-04-30 15:51:58 -0500

    housing predictors edits

--- R_scripts/0a_cleaning_housing.R
+++ R_scripts/0a_cleaning_housing.R
@@ -1,4 +1,5 @@
-# load packages ----
+# part 1 ----
+## load packages ----
 library(tidyverse)
 library(tidymodels)
 library(zipcodeR)
@@ -9,25 +10,44 @@ tidymodels_prefer()
 # set seed
 set.seed(1)
 
-# load outcome variable ----
+## load outcome variable ----
 outcome <- read_csv("data/raw/gay-ta.csv") %>% 
   janitor::clean_names() %>% 
-  transmute(zip_code = as.numeric(geoid10), index = totindex)
+  transmute(zip_code = as.factor(geoid10), index = totindex)
 
-# load predictor variables ----
-predictors <- read_csv("data/raw/ACSDP5Y2021.DP04-Data.csv", na = c("", "-", "NA", "null")) %>% 
-  janitor::clean_names() %>% 
-  mutate(zip_code = str_sub(name, 7, 100)) %>% 
-  mutate(across(where(is.character), as.numeric)) %>%
-  select(-geo_id, -name)
+## load predictor variables ----
+housing_predictors <- read_csv("data/raw/ACSDP5Y2021.DP04-Data.csv", na = c("", "-", "NA", "null"))
+#change name of the columns to be 2nd row in CSV
+names(housing_predictors) <- housing_predictors[1,]
+housing_predictors <- housing_predictors[-1,] %>% 
+  janitor::clean_names()
 
-# check missingness
-naniar::miss_var_summary(predictors) %>% 
-  filter(pct_miss < 100 & pct_miss > 1)
+##selecting necessary columns ----
+housing_predictors <- housing_predictors %>% 
+  #select only estimate columns, remove any annotation columns
+  select(geographic_area_name, contains("estimate_") & !contains("annotation")) %>%
+  #add variable with zip code
+  mutate(zip_code = str_sub(geographic_area_name, 7, 100)) %>% 
+  select(zip_code, everything(), -geographic_area_name) %>% 
+  #change everything to numeric (except zip code)
+  mutate(zip_code = factor(zip_code),
+         across(where(is.character), as.numeric))
+
+## check missingness ----
+naniar::miss_var_summary(housing_predictors) %>% 
+  filter(pct_miss > 1)
+#gross rent occupied units paying rent median dollars has more than 20% missing, should not be imputed, just removed
+housing_predictors <- housing_predictors %>% 
+  select(everything(), -estimate_gross_rent_occupied_units_paying_rent_median_dollars)
+
+## save cleaned dataset ----
+save(housing_predictors, file = "data/processed/housing_predictors.rda")
+
+# part 2 models ----
 
 # join data ----
 data <- outcome %>% 
-  inner_join(predictors, by = "zip_code") %>% 
+  inner_join(housing_predictors, by = "zip_code") %>% 
   recipe(index ~ ., data = .) %>% 
   update_role(zip_code, new_role = "identifier") %>% 
   step_zv(all_predictors()) %>% 
@@ -36,7 +56,7 @@ data <- outcome %>%
 
 # detect missing data
 not_matching <- outcome %>% 
-  anti_join(predictors) %>% 
+  anti_join(housing_predictors) %>% 
   pull(zip_code)
 
 map_df(not_matching, reverse_zipcode)
