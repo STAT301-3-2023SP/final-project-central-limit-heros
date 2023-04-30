f8a9e43eb9dc1374cfc3cc8cf874536c3a171db4
pascoesean <seanpascoe2024@u.northwestern.edu>
2023-04-29 23:37:07 -0500

    intro + outcome memo

--- R_scripts/parks_wrangling.R
+++ R_scripts/parks_wrangling.R
@@ -8,9 +8,14 @@ library(tidyverse)
 
 nanda_parks <- read_csv("data/raw/nanda_parks_2018/nanda_parks_zcta_2018_01P.csv")
 
-gayta <- read_csv("data/raw/gay-ta.csv")
+gayta <- read_csv("data/raw/gay-ta.csv") %>%
+  select(GEOID10, TOTINDEX)
 
-inner_join(gayta, nanda_parks, by = c( "GEOID10" = "zcta19")) %>%
-  nrow()
+gay_parks <- inner_join(gayta, nanda_parks, by = c( "GEOID10" = "zcta19")) 
 
 
+## missingness slay? ----
+
+naniar::vis_miss(gay_parks)
+
+# no <3 
