2b015a08d8e10af28c23bd3930c4daf939b152b5
vnevirkov <you@example.com>
2023-04-30 20:44:11 -0500

    add commuter to combined

--- R_scripts/1_predictor_joining.R
+++ R_scripts/1_predictor_joining.R
@@ -6,6 +6,7 @@ load("data/processed/demographic_data.rda")
 load("data/processed/housing_predictors.rda")
 load("data/processed/income_data.rda")
 load("data/processed/parks_data.rda")
+load("data/processed/commuter_data.rda")
 
 # combine datasets ----
 ## load outcome variable ----
@@ -17,7 +18,8 @@ combined_datasets <- outcome %>%
   inner_join(census_demographic, by = "zip_code") %>% 
   inner_join(housing_predictors, by = "zip_code") %>% 
   inner_join(census_income, by = "zip_code") %>% 
-  inner_join(nanda_parks, by = "zip_code")
+  inner_join(nanda_parks, by = "zip_code") %>% 
+  inner_join(census_commuter, by = "zip_code")
 
 # save ----
 save(combined_datasets, file = "data/processed/combined_datasets.rda")
