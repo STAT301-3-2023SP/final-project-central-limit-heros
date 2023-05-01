bb98336ec356d56f5b4f2baed40061f05e788f87
Dylan Yan <dylanyan@dhcp-10-105-27-50.wireless.northwestern.private>
2023-04-30 21:49:55 -0500

    added data splits/folds

---
+++ R_scripts/1b_data_splitting.R
@@ -0,0 +1,24 @@
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# set seed
+set.seed(1)
+
+# load data ----
+load("data/processed/combined_datasets.rda")
+
+# split data ----
+split <- initial_split(combined_datasets, prop = 0.8, strata = gayborhood_index)
+
+train <- training(split)
+test <- testing(split)
+
+# fold data ----
+folds <- vfold_cv(train, v = 10, repeats = 5, strata = gayborhood_index)
+
+# save ----
+save(split, folds, file = "data/processed/split_data.rda")
