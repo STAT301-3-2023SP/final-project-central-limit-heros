1e36d8d2eb8acbfcdb540c626390f3f735d28f75
pascoesean <seanpascoe2024@u.northwestern.edu>
2023-05-14 23:09:40 -0500

    commit new data split

--- R_scripts/0f_Outcome_Var_Analysis.R
+++ R_scripts/0f_Outcome_Var_Analysis.R
@@ -5,6 +5,7 @@
 ## loading packages + data ----
 
 library(tidyverse)
+library(tidymodels)
 library(patchwork)
 
 # contains our outcome variable, `totindex`
@@ -30,4 +31,47 @@ p2 <- ggplot(gayta, aes(x = log10(totindex))) +
   theme_minimal()
 
 p1 + p2 + plot_annotation(title = "Log Transformation Improves Target Variable Normality")
+
+
+# THIS CODE IS JUST HERE TO PROVE THAT THE LOGIT TRANSFORM IS *U*&Y*^*)%^&)
+
+recipe(totindex ~., data = gayta) %>%
+  step_mutate(totindex = totindex/100) %>%
+  step_logit(totindex, offset = 0.04) %>%
+  prep() %>%
+  bake(new_data = NULL) %>%
+  ggplot(aes(x = totindex)) +
+  geom_density(fill = "rosybrown1") +
+  labs(title = "Transformed Distribution",
+       x = "Gayborhood Index (logit Transform)",
+       y = "Density") +
+  theme_minimal()
+
+
+#  YEO JOHNSON ALSO DOES A BAD JOB-- THIS IS BECAUSE WE HAVE A LOT OF 0 VALUES
+
+
+recipe(totindex ~., data = gayta) %>%
+  step_YeoJohnson(totindex) %>%
+  prep() %>%
+  bake(new_data = NULL) %>%
+  ggplot(aes(x = totindex)) +
+  geom_density(fill = "rosybrown1") +
+  labs(title = "Transformed Distribution",
+       x = "Gayborhood Index (yeo johnson Transform)",
+       y = "Density") +
+  theme_minimal()
+  
+
+gayta %>%
+  # tidied value was -0.478 
+  mutate(yeo1 = 1/sqrt(1 + totindex),
+         yeo = 2*(1 - yeo1)) %>%
+  ggplot(aes(x = yeo)) +
+  geom_density(fill = "rosybrown1") +
+  labs(title = "Transformed Distribution",
+       x = "Gayborhood Index (Yeo Johnson Transform)",
+       y = "Density") +
+  theme_minimal()
+  
 
--- R_scripts/1b_data_splitting.R
+++ R_scripts/1b_data_splitting.R
@@ -11,14 +11,21 @@ set.seed(1)
 # load data ----
 load("data/processed/combined_datasets.rda")
 
+# log10 transform ----
+
+combined_datasets_wlog <- combined_datasets %>%
+  mutate(log_gbdex = log10(gayborhood_index)) %>%
+  select(!gayborhood_index)
+
 # split data ----
-split <- initial_split(combined_datasets, prop = 0.8, strata = gayborhood_index)
+split <- initial_split(combined_datasets_wlog, prop = 0.8, strata = log_gbdex)
 
 train <- training(split)
 test <- testing(split)
 
 # fold data ----
-folds <- vfold_cv(train, v = 8, repeats = 5, strata = gayborhood_index)
+folds <- vfold_cv(train, v = 8, repeats = 5, strata = log_gbdex)
 
 # save ----
 save(folds, split, train, file = "data/processed/split_data.rda")
+save(test, file = "data/processed/test_data.rda")
