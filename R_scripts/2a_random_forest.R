7bd84fb305963ac198ef5a3b90ce055121b0c488
vnevirkov <you@example.com>
2023-05-17 22:30:03 -0500

    preparation for random forest

---
+++ R_scripts/2a_random_forest.R
@@ -0,0 +1,88 @@
+#load packages ----
+library(tidyverse)
+library(tidymodels)
+library(tictoc)
+library(doParallel)
+
+# handle common conflicts
+tidymodels_prefer()
+
+#load required datasets ----
+load("data/processed/split_data.rda")
+
+# Define model ----
+rf_model <- rand_forest(
+  mode = "regression",
+  mtry = tune(),
+  min_n = tune()) %>%
+  set_engine("ranger")
+
+#check tuning parameters
+rf_params <- hardhat::extract_parameter_set_dials(rf_model)
+rf_params
+
+#mtry needs to be specified
+rf_params <- rf_params %>% 
+#  update(mtry = mtry(range = c(1,13)),
+         min_n = min_n())
+
+# define tuning grid ----
+rf_grid <- grid_regular(rf_params, levels = 5)
+
+# workflow ----
+rf_wflow <- workflow() %>% 
+  add_model(rf_model) %>% 
+#  add_recipe(fire_recipe) NEED EDIT
+
+# Tuning/fitting ----
+
+tic("rf")
+## Set up parallel processing ----
+#using 3 cores
+cl <- makePSOCKcluster(3)
+registerDoParallel(cl)
+
+# Place tuning code in here
+rf_tune <- rf_wflow %>% 
+  tune_grid(
+    resamples = fire_folds,
+    grid = rf_grid,
+    control = keep_pred,
+    metrics  = fire_metrics
+  )
+
+#end parallel processing
+stopCluster(cl)
+
+toc(log = TRUE)
+
+# save runtime info
+time_log <- tic.log(format = FALSE)
+
+rf_tictoc <- tibble(
+  model = time_log[[1]]$msg,
+  start_time = time_log[[1]]$tic, 
+  end_time = time_log[[1]]$toc,
+  runtime = end_time - start_time
+)
+
+# Write out results & workflow
+save(rf_wflow, rf_tune, rf_tictoc, file = "results/rf.rda")
+
+#load("results/elastic_net.rda")
+#collect_metrics(rf_tune)
+
+# How many of the indices are actually 0? ----
+load("data/processed/combined_datasets.rda")
+
+combined_datasets %>% 
+  filter(gayborhood_index == 0) %>% 
+  nrow()
+
+combined_datasets %>% nrow()
+
+
+#log train - is it normally distributed?
+train %>% 
+  ggplot(aes(x = log_gbdex))+
+  geom_density()
