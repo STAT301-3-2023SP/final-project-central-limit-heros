bf32ebda5ae592fa49efac95652a3867629cc0d4
pascoesean <seanpascoe2024@u.northwestern.edu>
2023-06-02 22:58:55 -0500

    add keras, pls glmnet, and start final report

--- R_scripts/2x_model_fitting/2d_2_elastic_net_PLS.R
+++ R_scripts/2x_model_fitting/2d_2_elastic_net_PLS.R
@@ -1,8 +1,6 @@
 ########################################################################################################
-# K-Nearest Neighbors Model Fitting ----
+# Elastic Net Fitting: PCA EDITION ----
 ########################################################################################################
-
-# K-nearest neighbors <3
 
 ########################################################################################################
 # LOAD PACKAGES + DATA ----
@@ -21,37 +19,35 @@ set.seed(702)
 # RECIPE ----
 ########################################################################################################
 
-
-knn_recipe <- recipe(gayborhood_index ~ ., data = train) %>%
+en_recipe <- recipe(gayborhood_index ~ ., data = train) %>%
   step_nzv(all_predictors()) %>%
   update_role(zip_code, new_role = "id") %>%
   step_impute_knn(all_numeric_predictors()) %>%
+  step_dummy(all_nominal_predictors()) %>%
   step_normalize(all_numeric_predictors()) %>%
-  step_dummy(all_nominal_predictors()) 
+  step_interact(~all_predictors():all_predictors()) %>%
+  step_pls(all_predictors(), num_comp = tune(), outcome = "gayborhood_index")
 
 
 ########################################################################################################
 # MODEL + WORKLOW SPECS ----
 ########################################################################################################
 
-knn_spec <-
-  nearest_neighbor(neighbors = tune(), dist_power = tune()) %>%
-  set_engine('kknn') %>%
-  set_mode('regression')
+en_spec <-
+  linear_reg(penalty = tune(), mixture = tune()) %>%
+  set_engine('glmnet')
 
-knn_workflow <- workflow() %>% 
-  add_model(knn_spec) %>% 
-  add_recipe(knn_recipe)
-
+en_workflow <- workflow() %>% 
+  add_model(en_spec) %>% 
+  add_recipe(en_recipe)
 
 ########################################################################################################
 # FITTING + TUNING ----
 ########################################################################################################
 
-knn_grid <- extract_parameter_set_dials(knn_workflow) %>%
-  update(neighbors = neighbors(range = c(5,40)),
-         dist_power = dist_power(range = c(1,4))) %>%
-  grid_regular(levels = 3)
+en_grid <- extract_parameter_set_dials(en_workflow) %>% 
+  update(num_comp = num_comp(c(1, 30))) %>%
+  grid_regular(levels = 5)
 
 ctrl_grid <- control_resamples(verbose = TRUE, save_pred = TRUE, save_workflow = TRUE)
 
@@ -60,29 +56,27 @@ ctrl_bayes <- control_bayes(verbose = TRUE, save_pred = TRUE, save_workflow = TR
 metrics <- metric_set(rmse, ccc)
 
 tic.clearlog()
-tic("KNN")
+tic("Elastic Net")
 
-knn_tuned <- knn_workflow %>%
+en_tuned <- en_workflow %>%
   tune_grid(
     resamples = folds, 
-    grid = knn_grid,
+    grid = en_grid,
     control = ctrl_grid,
     metrics = metrics
   )
-
-save(knn_tuned, file = "results/model_fits/knn_tuned.rda")
 
 toc(log = TRUE)
 
 # save runtime info
 
-knn_time_log <- tic.log(format = FALSE)
+en_time_log <- tic.log(format = FALSE)
 
-elapsed_time <- knn_time_log[[1]]$toc - knn_time_log[[1]]$tic
+elapsed_time <- en_time_log[[1]]$toc - en_time_log[[1]]$tic
 
-knn_time_data <- tribble(
+en_time_data <- tribble(
   ~ "model", ~"elapsed_time_s", ~"grid_length", ~"folds", ~"repeats", ~"recipes",
-  "K-Nearest Neighbors", elapsed_time, nrow(knn_grid), 8, 5, 1
+  "Elastic Net", elapsed_time, nrow(en_grid), 8, 5, 1
 ) %>%
   mutate(avg_time_per_model_ms = (1000*elapsed_time_s)/(grid_length*folds*repeats*recipes))
 
@@ -90,27 +84,31 @@ knn_time_data <- tribble(
 # NOW DO BAYES
 ########################################################################
 
-knn_bayes <- knn_workflow %>%
+en_bayes_pca <- en_workflow %>%
   # iterative tuning with `tune_bayes()`
   tune_bayes(resamples = folds,
-             initial = knn_tuned,
+             initial = en_tuned,
              control = ctrl_bayes,
-             iter = 7)
-
-save(knn_bayes, file = "results/model_fits/knn_bayes.rda")
+             iter = 10)
 
 ########################################################################
 ## HOW LONG DID BAYESIAN ITERATION TAKE?
 
-knn_time_log <- tic.log(format = FALSE)
+en_time_log <- tic.log(format = FALSE)
 
-elapsed_time <- knn_time_log[[1]]$toc - knn_time_log[[1]]$tic
+elapsed_time <- en_time_log[[1]]$toc - en_time_log[[1]]$tic
 
-knn_time_data <- knn_time_data %>%
+en_pca_time_data <- en_time_data %>%
   mutate(Bayesian_time_s = elapsed_time,
-         iterations = 7,
+         iterations = 10,
          bayesian_per_iter = Bayesian_time_s/iterations)
 
 # save time data
-save(knn_time_data, file = "results/model_times/knn_time_data.rda")
+save(en_pca_time_data, file = "results/model_times/en_pca_time_data.rda")
+
+########################################################################
+
+# Save model objects
+
+save(en_bayes_pca, file = "results/model_fits/en_pca_model.rda")
 
--- R_scripts/2x_model_fitting/2e_NeuralNet_Keras.R
+++ R_scripts/2x_model_fitting/2e_NeuralNet_Keras.R
@@ -1,8 +1,14 @@
 ########################################################################################################
-# K-Nearest Neighbors Model Fitting ----
+# Neural Network Fitting: Keras ----
 ########################################################################################################
 
-# K-nearest neighbors <3
+# NOTE:: THIS REQUIRES INSTALLATION OF TENSORFLOW + KERAS
+# YOU CAN LOOK STUFF UP ONLINE
+# USE MINICONDA IF YOU WANT IT TO WORK IDK WHY BUT IT DID
+
+# MUST ALSO RUN `tensorflow::install_tensorflow()` for this to work
+# NOTE:: THIS WILL RESTART YOUR R SESSION
+
 
 ########################################################################################################
 # LOAD PACKAGES + DATA ----
@@ -17,40 +23,43 @@ doMC::registerDoMC(cores = 6) # Vlad u will have to do the other thing for pcs <
 load(file = "data/processed/split_data.rda")
 set.seed(702)
 
+reticulate::use_condaenv("r-reticulate") # this is the name of my miniconda env with tensorflow installed
+
+# more info on how to install/get this to work here: https://tensorflow.rstudio.com/install/
+
 ########################################################################################################
 # RECIPE ----
 ########################################################################################################
 
 
-knn_recipe <- recipe(gayborhood_index ~ ., data = train) %>%
+mlp_recipe <- recipe(gayborhood_index ~ ., data = train) %>%
   step_nzv(all_predictors()) %>%
   update_role(zip_code, new_role = "id") %>%
   step_impute_knn(all_numeric_predictors()) %>%
-  step_normalize(all_numeric_predictors()) %>%
-  step_dummy(all_nominal_predictors()) 
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_numeric_predictors()) 
 
 
 ########################################################################################################
 # MODEL + WORKLOW SPECS ----
 ########################################################################################################
 
-knn_spec <-
-  nearest_neighbor(neighbors = tune(), dist_power = tune()) %>%
-  set_engine('kknn') %>%
+mlp_spec <- mlp(hidden_units = tune(), penalty = tune()) %>%
+  set_engine('keras') %>%
   set_mode('regression')
 
-knn_workflow <- workflow() %>% 
-  add_model(knn_spec) %>% 
-  add_recipe(knn_recipe)
+mlp_workflow <- workflow() %>% 
+  add_model(mlp_spec) %>% 
+  add_recipe(mlp_recipe)
 
 
 ########################################################################################################
 # FITTING + TUNING ----
 ########################################################################################################
 
-knn_grid <- extract_parameter_set_dials(knn_workflow) %>%
-  update(neighbors = neighbors(range = c(5,40)),
-         dist_power = dist_power(range = c(1,4))) %>%
+mlp_grid <- extract_parameter_set_dials(mlp_workflow) %>%
+  update(hidden_units = hidden_units(range = c(1,10)),
+         penalty = penalty(range = c(-8, 0))) %>%
   grid_regular(levels = 3)
 
 ctrl_grid <- control_resamples(verbose = TRUE, save_pred = TRUE, save_workflow = TRUE)
@@ -62,27 +71,27 @@ metrics <- metric_set(rmse, ccc)
 tic.clearlog()
 tic("KNN")
 
-knn_tuned <- knn_workflow %>%
+mlp_tuned <- mlp_workflow %>%
   tune_grid(
     resamples = folds, 
-    grid = knn_grid,
+    grid = mlp_grid,
     control = ctrl_grid,
     metrics = metrics
   )
 
-save(knn_tuned, file = "results/model_fits/knn_tuned.rda")
+save(mlp_tuned, file = "results/model_fits/mlp_tuned.rda")
 
 toc(log = TRUE)
 
 # save runtime info
 
-knn_time_log <- tic.log(format = FALSE)
+mlp_time_log <- tic.log(format = FALSE)
 
-elapsed_time <- knn_time_log[[1]]$toc - knn_time_log[[1]]$tic
+elapsed_time <- mlp_time_log[[1]]$toc - mlp_time_log[[1]]$tic
 
-knn_time_data <- tribble(
+mlp_time_data <- tribble(
   ~ "model", ~"elapsed_time_s", ~"grid_length", ~"folds", ~"repeats", ~"recipes",
-  "K-Nearest Neighbors", elapsed_time, nrow(knn_grid), 8, 5, 1
+  "K-Nearest Neighbors", elapsed_time, nrow(mlp_grid), 8, 5, 1
 ) %>%
   mutate(avg_time_per_model_ms = (1000*elapsed_time_s)/(grid_length*folds*repeats*recipes))
 
@@ -90,27 +99,27 @@ knn_time_data <- tribble(
 # NOW DO BAYES
 ########################################################################
 
-knn_bayes <- knn_workflow %>%
+mlp_bayes <- mlp_workflow %>%
   # iterative tuning with `tune_bayes()`
   tune_bayes(resamples = folds,
-             initial = knn_tuned,
+             initial = mlp_tuned,
              control = ctrl_bayes,
              iter = 7)
 
-save(knn_bayes, file = "results/model_fits/knn_bayes.rda")
+save(mlp_bayes, file = "results/model_fits/mlp_bayes.rda")
 
 ########################################################################
 ## HOW LONG DID BAYESIAN ITERATION TAKE?
 
-knn_time_log <- tic.log(format = FALSE)
+mlp_time_log <- tic.log(format = FALSE)
 
-elapsed_time <- knn_time_log[[1]]$toc - knn_time_log[[1]]$tic
+elapsed_time <- mlp_time_log[[1]]$toc - mlp_time_log[[1]]$tic
 
-knn_time_data <- knn_time_data %>%
+mlp_time_data <- mlp_time_data %>%
   mutate(Bayesian_time_s = elapsed_time,
          iterations = 7,
          bayesian_per_iter = Bayesian_time_s/iterations)
 
 # save time data
-save(knn_time_data, file = "results/model_times/knn_time_data.rda")
+save(mlp_time_data, file = "results/model_times/mlp_time_data.rda")
 
