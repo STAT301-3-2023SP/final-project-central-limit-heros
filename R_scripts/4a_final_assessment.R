473ac256f34d872a7c7162e65ccdc37775a5a8a2
pascoesean <seanpascoe2024@u.northwestern.edu>
2023-06-08 19:17:10 -0500

    could be gayer ft. cer preds alone

--- R_scripts/4a_final_assessment.R
+++ R_scripts/4a_final_assessment.R
@@ -5,14 +5,23 @@
 ##load packages ----
 library(tidyverse)
 library(tidymodels)
+library(rules) # for cubist ensemble
+library(stacks)
 
 # handle common conflicts
 tidymodels_prefer()
 
 ## load model results
 load("data/processed/test_data_lasso.rda")
+load("data/processed/split_data_lasso.rda")
 load("results/model_fits/fit_stack.rda")
+load("results/model_fits/cer_bayes.rda")
 # our winning model fit was our 3 member ensemble, so we'll use that to get stuff out
+
+########################################################################################################
+# ENSEMBLE MODEL ----
+########################################################################################################
+
 
 ensemble_preds <- test %>%
   bind_cols(predict(model_stack_blended_fit, .)) %>%
@@ -82,3 +91,64 @@ ccc_by_member <- map(member_preds, ccc_vec, truth = member_preds$gayborhood_inde
 
 save(ccc_by_member, rmse_by_member, file = "results/predictions/metrics_by_member.rda")
 #load("results/predictions/metrics_by_member.rda")
+
+
+# cubist might actaully be better
+########################################################################################################
+# CUBIST ALONE ----
+########################################################################################################
+
+# This is to prove that we CAN fit the model with RMSE = 3.2 in the ensemble; but its grasping
+# at straws (overfitting to the test data)
+
+cer_fit <- cer_bayes %>%
+  extract_workflow() %>%
+  # this is model 23 (the lowest rmse component of our ensemble)
+  finalize_workflow(c(committees = 100, neighbors = 4, max_rules = 250) %>% as_tibble_row()) %>%
+  fit(train) 
+
+cer_fit <- cer_bayes %>%
+  extract_workflow() %>%
+  finalize_workflow(select_best(cer_bayes, metric = "rmse")) %>%
+  fit(train) 
+
+
+cer_preds <- test %>%
+  bind_cols(predict(cer_fit, .)) %>%
+  select(.pred, gayborhood_index) %>%
+  mutate(predictions = VGAM::yeo.johnson(.pred, lambda = -0.268, inverse = T),
+         true_gayborhood_index = VGAM::yeo.johnson(gayborhood_index, lambda = -0.268, inverse = T)) %>%
+  select(predictions, true_gayborhood_index) %>%
+  mutate(residuals = true_gayborhood_index-predictions,
+         sq_resid = (residuals)^2) #%>%
+#summarise(mean = mean(sq_resid),
+#          rmse = sqrt(mean))
+
+# Save predictions
+
+save(cer_preds, file = "results/predictions/cer_preds.rda")
+#load("results/predictions/ensemble_preds.rda")
+
+
+cer_preds %>% 
+  yardstick::rmse(truth = true_gayborhood_index, estimate = predictions)
+
+cer_preds %>% 
+  yardstick::ccc(truth = true_gayborhood_index, estimate = predictions)
+
+# plot residuals against fitted values
+
+cer_preds %>%
+  ggplot(aes(x = predictions, y = residuals)) +
+  geom_point(alpha = 0.3) +
+  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
+  theme_minimal()
+
+
+cer_preds %>%
+  ggplot(aes(x = true_gayborhood_index, y = predictions)) +
+  geom_abline(slope = 1, color = "red", linetype = "dashed") +
+  geom_point() +
+  coord_obs_pred() +
+  theme_minimal()
+
